a522b841047826206a2d8be9ee2d635fea9a917c
jasim-n <muhammadsr972@gmail.com>
2021-05-01 22:40:12 +0500

    a

---
+++ src/mycomponentss/display.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const display =()=>{
+    
+ 
+    return(<h1>display </h1>);
+}
+export default display;
+
---
+++ src/mycomponentss/form.jsx
@@ -0,0 +1,86 @@
+
+import React, { Component } from 'react';
+import {Link} from "react-router-dom";
+
+
+class Form extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			firstname: '',
+			password: '',
+			name:''
+		}
+	}
+
+	handleFirstnameChange = event => {
+		this.setState({
+			firstname: event.target.value
+		})
+	}
+
+
+    
+
+	handleLastNameChange = event => {
+		this.setState({
+			name: event.target.value
+		})
+	}
+    handlepasswordChange = event => {
+		this.setState({
+			password: event.target.value
+		})
+	}
+
+
+	handleSubmit = event => {
+		alert(`${this.state.name} ${this.state.password} ${this.state.ab}`)
+        
+		event.preventDefault()
+	}
+   
+
+	render() {
+        
+        const mystyleForm={
+            border:"5px grey solid",
+            backgroundColor: "lightgrey",
+            margin: "0 23%",
+            padding: "10px",
+            
+            textAlign: "center",
+
+        };
+		const { firstname, name, password} = this.state
+       
+		return (
+            <div >
+			<form onSubmit={this.handleSubmit} style={mystyleForm}>
+				<div>
+					<label>Firstname </label>
+					<input
+						type="text"
+						value={firstname}
+						onChange={this.handleFirstnameChange}
+					/>
+				</div>
+				<div>
+                    <label>Last name</label>
+                    <input type="text" value={name} onChange={this.handleLastNameChange}></input>
+                </div>
+                <div>
+                    <label>password</label>
+                    <input type="password" value={password} onChange={this.handlepasswordChange}></input>
+                </div>
+				
+				<button type="submit"><li><Link to={"/display"  } >submit</Link></li></button>
+			</form></div>
+		)
+	}
+}
+
+export default Form
+
+
